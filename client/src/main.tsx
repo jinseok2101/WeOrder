2911c1f6834ccbe3c6cf0fd6b7ac9a9cfa2de5f1
jinseok2101 <jslee2101@naver.com>
2026-03-24 17:02:49 +0900

    Patch ReactDOM preconnect for legacy react 18 compatibility

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as ReactDOMServer from 'react-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
+
+// React 18.2 이하 버전에서 react-naver-maps 0.2.0(최신)을 돌릴 때 발생하는 preconnect 누락 버그(에러) 방어막
+// @ts-ignore
+if (!ReactDOM.preconnect) ReactDOM.preconnect = () => {};
+// @ts-ignore
+if (!ReactDOMServer.preconnect) ReactDOMServer.preconnect = () => {};
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,9 +22,7 @@ const queryClient = new QueryClient({
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 );
