015c3aeb3d32360859b1da03f8a29d7981e9881e
jinseok2101 <jslee2101@naver.com>
2026-03-24 17:05:39 +0900

    Fix typescript types for react-naver-maps v0.1.3

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,12 +5,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
 
-// React 18.2 이하 버전에서 react-naver-maps 0.2.0(최신)을 돌릴 때 발생하는 preconnect 누락 버그(에러) 방어막
-// @ts-ignore
-if (!ReactDOM.preconnect) ReactDOM.preconnect = () => {};
-// @ts-ignore
-if (!ReactDOMServer.preconnect) ReactDOMServer.preconnect = () => {};
-
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
